0c24ba371dc403dc6f9d87bcb7bbcc26404f47de
Nisha0212 <68599027+Nisha0212@users.noreply.github.com>
2021-10-21 11:43:53 +0530

    Update ast.ml
    Added additional printing functions for AST

--- ast.ml
+++ ast.ml
@@ -80,3 +80,107 @@ let check_primitive sc_specifier cmpd_typ variable_name expr =
       | _ -> (sc_specifier, cmpd_typ, variable_name, (-1, -1), expr) 
     )
     | _ -> (sc_specifier, cmpd_typ, variable_name, (-1, -1), expr)
+
+
+
+(*priting functions*)
+
+let print_oper = function
+    Add -> "+"
+  | Sub -> "-"
+  | Mul -> "*"
+  | Div -> "/"
+  | Equal -> "=="
+  | Not_equal -> "!="
+  | Lt -> "<"
+  | Lte -> "<="
+  | Gt -> ">"
+  | Gte -> ">="
+  | And -> "AND"
+  | Or -> "OR" 
+  | Mod -> "%"
+
+
+let print_uoper = function
+  (* Neg -> "-" *)
+  | Not -> "!"
+
+let print_storage_class = function
+    | Const -> "const"
+    | Static -> "static"
+    | Rename -> "rename"
+    | Noexpr -> " "
+    | Normal -> ""  
+
+let rec print_expr_string = function
+  Int(l) -> string_of_int l
+  | Floatlit(l) -> string_of_float l
+  | True -> "true"
+  | False -> "false"
+  | Strlit(l) -> "\"" ^ (String.escaped l) ^ "\""
+  | MatrixLit(_) -> "matLit"
+  | Id(s) -> s
+  | Binop(e1, o, e2) ->print_expr_string e1 ^ " " ^ print_oper o ^ " " ^ print_expr_string e2
+  | Unop(o, e) -> print_uoper o ^ print_expr_string e
+  | Asgn(v, e) -> v ^ " = " ^ print_expr_string e
+  | FunCall(f, el) -> f ^ "(" ^ String.concat ", " (List.map print_expr_string el) ^ ")"
+  | MatrixRow(s,e) -> s ^ ".row"
+  | MatrixElem(s, r, c) -> s ^ "[" ^ print_expr_string r ^ "]" ^ "[" ^ print_expr_string c ^ "]"
+  | MatrixModify(s, (e1,e2), e3) -> s ^ "[" ^ print_expr_string(e1) ^ ", " ^ print_expr_string(e2) ^ "]" ^ " = " ^ print_expr_string(e3)
+  | StructAccess((s1,s2)) -> s1 ^ s2
+  | StructAsgn((s1,s2,e)) -> s1 ^ "." ^ s2 ^ " = " ^ print_expr_string(e) 
+  | StructMatrixAccess(s1,s2,(e1,e2)) -> s1 ^ s2 ^ "[" ^ print_expr_string(e1) ^ ", " ^ print_expr_string(e2) ^ "]"
+  | StructMatrixModify((s1,s2,(e1,e2),e3)) -> s1 ^ s2 ^ "[" ^ print_expr_string(e1) ^ ", " ^ print_expr_string(e2) ^ "]" ^ " = " ^ print_expr_string(e3)
+  | Noexpr -> ""
+  | Null -> ""
+
+let print_typ_str = function
+  Void -> "void"
+  | Int -> "int"
+  | Bool -> "bool"
+  | Float -> "float"
+  | String -> "string"
+  | Matrix -> "matrix"
+
+
+let print_cmpdtyp_info = function
+    Datatype(d) -> print_typ_str d
+  | Struct(s) -> "struct" ^ s
+
+
+let rec print_stmt_string = function
+  Block(stmts) -> "{\n" ^ String.concat "" (List.map print_stmt_string stmts) ^ "}\n"
+  | Expr(expr) -> print_expr_string expr ^ ";\n";
+  | Return(expr) -> "return " ^ print_expr_string expr ^ ";\n";
+  | If(e, s, Block([])) -> "if (" ^ print_expr_string e ^ ")\n" ^ print_stmt_string s
+  | If(e, s1, s2) ->  "if (" ^ print_expr_string e ^ ")\n" ^  print_stmt_string s1 ^ "else\n" ^ print_stmt_string s2
+  | Loop(e1, e2, s) -> "loop (" ^ print_expr_string e1  ^ " ; " ^ print_expr_string e2 ^ " ; " ^ ") " ^ print_stmt_string s
+  | Break -> "break"
+  | Continue -> "continue"
+  
+
+let string_of_vdecl (t, id) = print_cmpdtyp_info t ^ " " ^ id ^ ";\n"
+let string_of_vdecl (s,t, id,_,expr) = print_storage_class s ^ print_cmpdtyp_info t ^ " " ^ id ^ " " ^ print_expr_string expr ^ ";\n"
+
+  let string_of_fdecl fdecl =
+    let thirdoffive = fun (_,_,y,_, _) -> y in
+    print_cmpdtyp_info fdecl.ret_type ^ " " ^
+    fdecl.fname ^ "(" ^ String.concat ", " (List.map thirdoffive fdecl.args) ^
+    ")\n{\n" ^
+    String.concat "" (List.map string_of_vdecl fdecl.local_vars) ^
+    String.concat "" (List.map print_stmt_string fdecl.body) ^
+    "}\n"
+  
+  let string_of_struct_decl s =
+    let vdecls = String.concat "" (List.map string_of_vdecl s.members) in
+    "struct " ^ s.name ^ "{\n" ^
+      vdecls ^
+    "}\n"
+  
+    let print_program_string program =
+      let vars = program.globals
+      and funcs = program.funcs
+      and structs = program.structs in
+      String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+      String.concat "" (List.map string_of_struct_decl structs) ^ "\n" ^
+      String.concat "\n" (List.map string_of_fdecl funcs)  
