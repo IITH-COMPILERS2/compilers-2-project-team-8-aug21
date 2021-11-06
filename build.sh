ab9b5ad0c2ea74242c037886a75c421ea8fc6b49
Prime-Nemesis <surajg2002@gmail.com>
2021-11-06 21:59:05 +0530

    modified build file to complie and link in proper order

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-ocamllex lexer.mll
-ocamlyacc parser.mly
-ocamlc -c ast.ml parser.mli parser.ml
-ocamlopt ast.ml semant.ml parser.ml lexer.ml main.ml
+ocamllex lexer.mll                      # lexer.ml is output
+ocamlyacc parser.mly                    #parser.ml and parser.mli are outputs
+ocamlc -c ast.ml parser.mli parser.ml   # ast.cmi .cmo and parser.cmo .cmo generated
+ocamlopt ast.ml semant.ml parser.ml lexer.ml main.ml    # rest of files with a.out
