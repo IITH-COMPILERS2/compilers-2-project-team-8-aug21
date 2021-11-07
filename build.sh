4e90faa4bfb799ec35592f549f45b60da136658c
Gayala Manoj <65646563+manojgayala@users.noreply.github.com>
2021-11-07 16:31:38 +0530

    Removed unnecessary files
    .cmx .cmo .cmi .o files have been removed after executable is formed.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-ocamllex lexer.mll                      # lexer.ml is output
-ocamlyacc parser.mly                    #parser.ml and parser.mli are outputs
-ocamlc -c ast.ml parser.mli parser.ml   # ast.cmi .cmo and parser.cmo .cmo generated
-ocamlopt ast.ml semant.ml parser.ml lexer.ml main.ml    # rest of files with a.out
+ocamllex lexer.mll
+ocamlyacc parser.mly
+ocamlc -c ast.ml parser.mli parser.ml
+ocamlopt ast.ml semant.ml parser.ml lexer.ml main.ml
+rm *.cmo *.cmi *.cmx *.o
